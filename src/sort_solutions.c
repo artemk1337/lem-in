a185794143216752c3dfe42ff05d63a0ad7dc349
MaximSinyaev <sinyaevmaxim@gmail.com>
2020-03-03 16:13:02 +0300

    sort in pocess

--- src/sort_solutions.c
+++ src/sort_solutions.c
@@ -11,19 +11,59 @@
 /* ************************************************************************** */
 
 //#include "lemin.h"
-//
-//void sort_solutions(t_solution **solution)
-//{
-//	t_solution *begin;
-//	t_solution *temp;
-//	t_solution *iter;
-//
-//	if (*solution || !(*solution)->next)
-//		return ;
-////	begin = *solution;
-//	begin = (*solution)->next
-//	while (begin->next)
-//		temp = begin->next;
-//		while (temp->path_len < begin->path_len)
-//			temp
-//}
+
+typedef struct			s_solution
+{
+//	t_room				**arr;
+	unsigned int		path_len;
+	struct s_solution	*next;
+}						t_solution;
+
+void sort_solutions(t_solution **solution)
+{
+	t_solution *begin;
+	t_solution *temp[2];
+	t_solution *main_iter;
+	t_solution *iter;
+
+	if (!*solution || !(*solution)->next)
+		return ;
+	begin = (*solution);
+	main_iter = begin->next;
+	while (main_iter)
+	{
+		iter = begin;
+		while (iter->next != main_iter)
+		{
+			if (iter->next->path_len > main_iter->path_len)
+			{
+				temp[0] = iter->next;
+				iter->next = main_iter;
+				temp[1] = main_iter->next;
+				main_iter->next = temp[0];
+			}
+			iter = iter->next;
+		}
+		main_iter = main_iter->next;
+	}
+}
+
+#include <stdlib.h>
+
+int main()
+{
+	t_solution *ptr, *begin;
+
+	begin = ptr = malloc(sizeof(*ptr));
+	ptr->path_len = 8;
+	ptr->next = malloc(sizeof(*ptr));
+	ptr = ptr->next;
+	ptr->path_len = 8;
+	ptr->next = malloc(sizeof(*ptr));
+	ptr = ptr->next;
+	ptr->path_len = 6;
+	ptr->next = malloc(sizeof(*ptr));
+	ptr = ptr->next;
+	ptr->path_len = 7;
+	sort_solutions(&begin);
+}
