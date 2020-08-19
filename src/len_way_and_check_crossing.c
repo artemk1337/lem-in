4c79f78fec70b54ca6c6c8dc44cf1c7b77f7e8d8
Artem <artemkush1@gmail.com>
2020-08-19 11:12:06 -0700

    fix

--- src/len_way_and_check_crossing.c
+++ src/len_way_and_check_crossing.c
@@ -23,36 +23,23 @@ int		count_len_way(void)
 	len = 1;
 	cross_way = 0;
 	curr = g_lemin->finish;
-	//ft_putstr("CHECK\n");
-	// Считаю длину пути
 	prev = curr->prev;
 	while (prev)
 	{
-		//ft_putstr("Room: ");
-		//ft_putstr(prev->name);
-		//ft_putstr((prev->in) ? "_out": "");
-		//ft_putstr((prev->out) ? "_in": "");
-		//ft_putstr("\n");
 		if (prev == g_lemin->start)
 			break ;
 		len++;
-		// Если перешел из комнаты in в комнату out
 		if (curr->in && prev->out)
 		{
-			// Глоб
-			//ft_putstr("1\n");
 			neigh = curr->next;
 			while (neigh->room != prev)
 				neigh = neigh->next;
-			//ft_putstr((neigh) ? "YES\n": "NO\n");
 			neigh->global_toggle = 0;
-			//ft_putstr("2\n");
 			neigh = prev->next;
 			while (neigh->room != curr)
 				neigh = neigh->next;
 			neigh->global_toggle = 0;
 			cross_way = 1;
-			//ft_putstr("3\n");
 		}
 		curr = prev;
 		prev = curr->prev;
